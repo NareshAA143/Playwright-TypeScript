a892f360fe46ed6a68749094c5c25569aef9d12f
NareshAA143 <naresh75.nn@gmail.com>
2026-01-03 21:30:44 +0530

    new test added

--- src/test/steps/SauseDemoPagetest.spec.ts
+++ src/test/steps/SauseDemoPagetest.spec.ts
@@ -1,5 +1,7 @@
 import { Given,When,Then } from "@cucumber/cucumber";
 import { CustomWorld } from "../../hooks/world";
+import { expect } from "playwright/test";
+import Allure from "allure-js-commons";
 
 Given('User navigates to Sausedemo URL', async function (this: CustomWorld) {
     await this.sausdemoPage.NavigateToSausdemoPage();
@@ -18,4 +20,32 @@ Then('User sees products',async function (this: CustomWorld) {
 });
 Then('User clicks on AddToCart for specific products',async function (this: CustomWorld) {
     await this.sausdemoPage.AddSpecificProductsToCart();
+});
+Then('User filters By ATOZ', async function (this: CustomWorld) {
+    await this.sausdemoPage.filterByNameAtoZ();
+    const names=await this.sausdemoPage.getProductNames();
+    const sorted=[...names].sort();
+    console.log(sorted);
+    expect(names).toEqual(sorted);
+});
+Then('User filters By ZToA', async function (this: CustomWorld) {
+    await this.sausdemoPage.filterByNameZtoA();
+    const names=await this.sausdemoPage.getProductNames();
+    const sorted=[...names].sort().reverse();
+    console.log(sorted);
+    expect(names).toEqual(sorted);
+});
+Then('User filters By LowToHigh', async function (this: CustomWorld) {
+    await this.sausdemoPage.filterByPriceLowtoHigh();
+    const prices=await this.sausdemoPage.getProductPrices();
+    const sorted=[...prices].sort((a,b)=>a-b);
+    console.log(sorted);
+    expect(prices).toEqual(sorted);
+});
+Then('User filters By HighToLow', async function (this: CustomWorld) {
+    await this.sausdemoPage.filterByPriceHightoLow();
+    const prices=await this.sausdemoPage.getProductPrices();
+    const sorted=[...prices].sort((a,b)=>b-a);
+    console.log(sorted);
+    expect(prices).toEqual(sorted);
 });
