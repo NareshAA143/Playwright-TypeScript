e6ab9ee9d83aeba3d2c6bea295453bc2d8bcc31a
NareshAA143 <naresh75.nn@gmail.com>
2026-01-03 18:34:36 +0530

    new branch added

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,14 +22,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['github']],
+  reporter: [['html',{outputFolder:'My-Report', open:'always'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: true,
+    headless: false,
     trace: 'retain-on-failure', 
     screenshot: 'only-on-failure',
     video: 'on',
@@ -38,10 +38,16 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+     {
+    name: 'chromium',
+    use: {
+      ...devices['Desktop Chrome'],
+      viewport: null,                
+      launchOptions: {
+        args: ['--start-maximized'], 
+      },
     },
+  },
 
     {
       name: 'firefox',
